45ae51c8b30a83189f04bc93d0dabbb396dd0013
rebear217 <r.e.beardmore@exeter.ac.uk>
2020-03-30 13:31:34 +0100

    updates to command window reporting
    this clarifies some reporting text delivered to the command window during execuation

--- corona/SIR/generateExemplars.m
+++ corona/SIR/generateExemplars.m
@@ -15,12 +15,15 @@ parameters.p(6) = parameters.positiveComplianceRate;
 
 %%
 
-runCorona(parameters)
+d = runCorona(parameters);
+disp(['Check: dead with isolation as a % of S(0) is ',num2str(100*d)]);
+
 my_export_fig('basicModelfullCompliance.pdf')
 
 %%
 
 parameters.timeIso = 0;
-runCorona(parameters)
+d = runCorona(parameters);
+disp(['Check: dead with no isolation as a % of S(0) is ',num2str(100*d)]);
 
 my_export_fig('basicModelNoIsolation.pdf')
